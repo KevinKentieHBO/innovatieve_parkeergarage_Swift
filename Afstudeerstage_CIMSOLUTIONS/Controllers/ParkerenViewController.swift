c02224b02bdbf41ff0b775d6a0229fb323016aee
KevinKentieHBO <kevin.kentie@student.hu.nl>
2021-03-25 08:32:35 +0100

    Initial commit

---
+++ Afstudeerstage_CIMSOLUTIONS/Controllers/ParkerenViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ParkerenViewController.swift
+//  Afstudeerstage_CIMSOLUTIONS
+//
+//  Created by Kevin Kentie on 24/03/2021.
+//
+
+import UIKit
+
+class ParkerenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Afstudeerstage_CIMSOLUTIONS/Models/Betaaltarief.swift
@@ -0,0 +1,33 @@
+//
+//  Betaaltarief.swift
+//  Afstudeerstage_CIMSOLUTIONS
+//
+//  Created by Kevin Kentie on 24/03/2021.
+//
+
+import Foundation
+
+struct Betaaltarief : Decodable {
+    let id: Int
+    let type: String
+    let waarde: Double
+}
+
+//haal alle betaaltarieven op
+func getTestAllTarieven(_ completion: @escaping ([Betaaltarief]) -> ()) {
+            if let url = URL(string: "http://localhost:8080/betaaltarief") {
+               URLSession.shared.dataTask(with: url) { data, response, error in
+                  if let data = data {
+
+                      do {
+                         let res = try JSONDecoder().decode([Betaaltarief].self, from: data)
+                        print(res)
+                        completion(res)
+                        return
+                      } catch let error {
+                         print(error)
+                      }
+                  }
+               }.resume()
+        }
+}
