3700eab8cf93d5a3c182cb988ff9e6f93db8395c
KevinKentieHBO <kevin.kentie@student.hu.nl>
2021-04-10 12:21:04 +0200

    Login wordt herkent en valideert.

--- Afstudeerstage_CIMSOLUTIONS/Controllers/LoginViewController.swift
+++ Afstudeerstage_CIMSOLUTIONS/Controllers/LoginViewController.swift
@@ -8,22 +8,49 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var gebruikersnaamInput: UITextField!
+    @IBOutlet weak var wachtwoordInput: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func login(_ sender: Any) {
+        if(gebruikersnaamInput.text == "" || wachtwoordInput.text == ""){
+            creareAlert(title: "Error:", message: "Vul alle velden in.")
+        }else{
+            checkLogin(gebruikersnaam: gebruikersnaamInput.text!, wachtwoord: wachtwoordInput.text!){
+                (array) in
+                DispatchQueue.main.async {
+                    if(array.Resultaat == "true"){
+                        let defaults = UserDefaults.standard
+                        defaults.set(array.Account_Id, forKey: "Account_Id")
+                        defaults.set(array.Account_Actief_Kenteken, forKey: "Actief_Kenteken")
+                        defaults.set(array.Account_Token, forKey: "Account_Token")
+                    }else{
+                        self.creareAlert(title: "Error:", message: "Gebruikersnaam en wachtwoord combinatie niet correct.")
+                    }
+                }
+            }
+        }
     }
-    */
-
+    
+    //Aanmaken van een alert om de error te tonen aan de gebruiker
+    func creareAlert(title: String, message: String)
+    {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Oke", style: .default, handler: {(action) in
+            alert.dismiss(animated: true, completion: nil)
+        }))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
 }
---
+++ Afstudeerstage_CIMSOLUTIONS/Models/Account.swift
@@ -0,0 +1,40 @@
+//
+//  Account.swift
+//  Afstudeerstage_CIMSOLUTIONS
+//
+//  Created by Kevin Kentie on 10/04/2021.
+//
+
+import Foundation
+
+struct Account : Decodable{
+    let Resultaat : String
+    let Account_Id : Int
+    let Account_Token : String
+    let Account_Actief_Kenteken : String
+}
+
+func checkLogin(gebruikersnaam : String, wachtwoord : String, _ completion: @escaping (Account) -> ()) {
+    
+    let herokuUrl : String = "https://javainnovatieveparkeergarage.herokuapp.com"
+    let localhostUrl : String = "http://localhost:8080"
+    
+    if let url = URL(string:"\(localhostUrl)/account/\(gebruikersnaam)/\(wachtwoord)") {
+       URLSession.shared.dataTask(with: url) { data, response, error in
+          if let data = data {
+            
+            let d = String(data: data,encoding: .utf8)
+            let decodedData = Data((d?.aesDecrypt()!.utf8)!)
+
+              do {
+                 let res = try JSONDecoder().decode(Account.self, from: decodedData)
+                print(res)
+                completion(res)
+                return
+              } catch let error {
+                 print(error)
+              }
+          }
+       }.resume()
+    }
+}
